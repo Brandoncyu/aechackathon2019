f401340c4b792c6fc0c6547fdad89060cd24b13f
Nate Holland <nate.holland2@gmail.com>
2019-03-30 14:20:02 -0700

    split out YelpParks.JS - now returning list of names, ratings and coordinates

--- yelp-stuff/script.js
+++ yelp-stuff/script.js
@@ -1,27 +1,11 @@
 
-const yelp = require('yelp-fusion');
 
-// Place holder for Yelp Fusion's API Key. Grab them
-// from https://www.yelp.com/developers/v3/manage_app
-const apiKey = '6NYOaiq8v9lYb4anGkNvmrRxSKejVfIlvJZjHkK__IQv2uAcn8xBp_6yW58coOfuUMwpp1Tjmmy3hUTjJ65DKOfuE8GF3qvwZPMDirzf88MWTPRbN2uNOI7hvb2fXHYx';
+const YelpParks = require('./YelpParks');
 
-const searchRequest = {
-  term:'park',
-  //location: 'san francisco, ca',
-  latitude: 47.6694956,
-  longitude:-122.31547389999999
-};
+let lat = 47.6694956
+let long = -122.31547389999999
 
-const client = yelp.client(apiKey);
+YelpParks.ParkSearch(lat,long);
 
 
-
-client.search(searchRequest).then(response => {
-  const firstResult = response.jsonBody.businesses[0];
-  const prettyJson = JSON.stringify(firstResult, null, 4);
-  console.log(prettyJson);
-  console.log(response)
-}).catch(e => {
-  console.log(e);
-});
 
