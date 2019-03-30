1f779ed56ce1fb30725906a135d08a3ea1e1a2f8
Petr Mitev <p.mitevpi@gmail.com>
2019-03-30 13:30:11 -0700

    module

--- wip/index.js
+++ wip/index.js
@@ -1,10 +1,3 @@
-// var Vibrant = require('node-vibrant')
-// Vibrant.from('https://maps.googleapis.com/maps/api/streetview?size=600x300&location=46.414382,10.013988&heading=151.78&pitch=-0.76&key=AIzaSyBiwsS0P3PVv4dr2sTs61i_9d-ICOsmSgw').getPalette()
-//   .then(palette => {
-//     console.log(palette);
-//     console.log(palette);
-//   }).catch(err => console.error(err));
-
 const ColorParse = require('./ColorParse');
 
 ColorParse.GetPalette().then(colors => console.log(colors));
