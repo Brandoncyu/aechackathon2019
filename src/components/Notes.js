5d1bc89466efe0e7c38f48d8004ca8301ac9a4ca
Brandon <brandoncyu@gmail.com>
2019-03-30 15:54:21 -0700

    added react-mic

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import Recorder from 'react-mp3-recorder'
+import React, { Component } from 'react';
+import { ReactMic } from 'react-mic';
 import {
   Button,
   Form,
@@ -11,40 +11,75 @@ import {
   Input
 } from 'reactstrap'
 
-const Notes = () => {
-  const onRecordingComplete = (blob) => {
-    console.log('recording', blob)
+class Notes extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      record: false,
+      blobs: []
+    }
   }
 
-  const onRecordingError = (err) => {
-    console.log('recording error', err)
+  startRecording = () => {
+    this.setState({
+      record: true
+    });
   }
 
-  return (<Container fluid={true}>
-    <Row>
-      <Col xl="3"></Col>
-      <Col>
-        <Form className="p-4" id="initial-form">
-          <FormGroup>
-            <Label htmlFor="email">Email</Label>
-            <Input type="email" name="email" id="email" placeholder="Verify your email address" />
-          </FormGroup>
-          <FormGroup>
-          <Label for="exampleText">Text Area</Label>
-          <Input type="textarea" name="text" id="exampleText" />
-        </FormGroup>
-          <Button color="primary">Find Your Route!</Button>
-          <br /><br />
-          <Recorder
-            onRecordingComplete={onRecordingComplete}
-            onRecordingError={onRecordingError}
-          />
-        </Form>
-        
-      </Col>
-      <Col xl="3"></Col>
-    </Row>
-  </Container>)
+  stopRecording = (e) => {
+    console.log(e)
+    this.setState({
+      record: false
+    });
+  }
+
+  onData(recordedBlob) {
+    // console.log('chunk of real-time data is: ', recordedBlob);
+  }
+
+  onStop = (recordedBlob) => {
+    // console.log('recordedBlob is: ', recordedBlob);
+    let newBlob = recordedBlob.blobURL
+    let newBlobs = [ ...this.state.blobs, newBlob ]
+    this.setState({
+      blobs: newBlobs
+    })
+  }
+
+  render(){
+    return (<Container fluid={true}>
+      <Row>
+        <Col xl="3"></Col>
+        <Col>
+          <Form className="p-4" id="initial-form">
+            <FormGroup>
+              <Label htmlFor="email">Email</Label>
+              <Input type="email" name="email" id="email" placeholder="Verify your email address" />
+            </FormGroup>
+            <FormGroup>
+              <Label for="exampleText">Text Area</Label>
+              <Input type="textarea" name="text" id="exampleText" />
+            </FormGroup>
+              <Button color="primary">Find Your Route!</Button>
+              <br /><br />
+
+              <ReactMic
+                record={this.state.record}
+                className="sound-wave"
+                onStop={this.onStop}
+                onData={this.onData}
+                strokeColor="#000000"
+                backgroundColor="#FFFFFF" />
+              <br />
+              <Button color="primary" onClick={this.startRecording}>Start</Button>
+              <Button color="primary" onClick={this.stopRecording}>Stop</Button>
+          </Form>
+
+        </Col>
+        <Col xl="3"></Col>
+      </Row>
+    </Container>
+  )}
 }
 
 export default Notes
