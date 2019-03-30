d4a383cf6ff7bb2eb2f492ed3fb364a9ee80f829
Petr Mitev <p.mitevpi@gmail.com>
2019-03-30 13:17:16 -0700

    decent color snap

---
+++ wip/ColorParse.js
@@ -0,0 +1,17 @@
+var Vibrant = require('node-vibrant')
+
+class ColorParse {
+  constructor() {}
+
+  static GetPalette() {
+    return new Promise(function (resolve, reject) {
+      Vibrant.from('https://maps.googleapis.com/maps/api/streetview?size=600x300&location=46.414382,10.013988&heading=151.78&pitch=-0.76&key=AIzaSyBiwsS0P3PVv4dr2sTs61i_9d-ICOsmSgw').getPalette()
+        .then(palette => {
+          console.log(palette);
+          resolve(palette);
+        }).catch(err => console.error(err));
+    })
+  }
+}
+
+module.exports = ColorParse;
