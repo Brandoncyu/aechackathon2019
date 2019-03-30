b67ca0ddfd845f209fece28bf8bd8c7dac447a97
Brandon <brandoncyu@gmail.com>
2019-03-30 11:37:44 -0700

    added reactstrap

--- src/App.js
+++ src/App.js
@@ -4,13 +4,24 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Take a stroll.
-          </p>
-        </header>
-      </div>
+      <form>
+        <fieldset>
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" id="email" value="email" />
+        </fieldset>
+        <fieldset>
+          <label htmlFor="time">Time</label>
+          <input type="number" name="time" id="time" value="time" />
+        </fieldset>
+        <fieldset>
+          <label htmlFor="email">Pace</label>
+          <select>
+            <option value="slow">Slow</option>
+            <option value="medium">Medium</option>
+            <option value="fast">Fast</option>
+          </select>
+        </fieldset>
+      </form>
     );
   }
 }
