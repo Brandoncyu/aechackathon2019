d7e55517b6690c485dd38addd0f73de54d26da77
Petr Mitev <p.mitevpi@gmail.com>
2019-03-30 14:00:07 -0700

    stash

--- wip/ColorParse.js
+++ wip/ColorParse.js
@@ -3,27 +3,30 @@ var Vibrant = require('node-vibrant')
 class ColorParse {
   constructor() {}
 
-  static BuildRequest(lat, long){
+  static BuildRequest(lat, long, heading) {
+    // let location = 'location=46.414382,10.013988&';
+    // let heading = 'heading=151.78&';
     let base = 'https://maps.googleapis.com/maps/api/streetview?';
-    let size = 'size=600x300&'
-    let location = 'location=46.414382,10.013988&';
-    let heading = 'heading=151.78&';
+    let size = 'size=800x400&'
+    let location = 'location=' + String(lat) + ',' + String(long) + '&';
+    let headingStr = 'heading=' + heading + '&' || 'heading=0.0&';
     let pitch = 'pitch=-0.76&'
     let key = 'key=AIzaSyBiwsS0P3PVv4dr2sTs61i_9d-ICOsmSgw';
 
-    return base+size+location+heading+pitch+key;
+    return base + size + location + headingStr + pitch + key;
   }
 
   static GetPalette() {
     let self = this;
     return new Promise(function (resolve, reject) {
-      let url = self.BuildRequest(46.414382, 10.013988);
+      let url = self.BuildRequest(46.414382, 10.013988, 151.78);
       Vibrant.from(url).getPalette()
         .then(palette => {
           resolve(palette);
         }).catch(err => console.error(err));
     })
   }
+
 }
 
 module.exports = ColorParse;
