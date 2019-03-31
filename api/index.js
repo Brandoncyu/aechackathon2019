c2eb6b806460b22e9a550c79dd4914146e50b9da
Petr Mitev <p.mitevpi@gmail.com>
2019-03-30 23:52:41 -0700

    module aggregation in index.js

--- api/index.js
+++ api/index.js
@@ -4,50 +4,30 @@ const YelpData = require('./YelpData');
 const WeatherData = require('./WeatherData');
 const RouteData = require('./RouteData');
 
-
-// YelpData.ParkSearch(47.6694956, -122.31547389999999).then(d => {
-//   console.log(d);
-// });
-
-// let points = RouteData.GetRandomPointGrid(-90.548630, 14.616599, 1, 10);
-// console.log(points);
-
-// let points = RouteData.GetPointGrid(-90.548630, 14.616599, 1, 0.1);
-// console.log(points);
-
-// RouteData.GetGraphData(47.660273, -122.409887, 1, 0.5, 0.6).then(x => {
-//   console.log(x);
-//   console.log(x);
-// });
-
-RouteData.GetGraph(47.660273, -122.409887, 1, 0.5, 0.6).then(x => {
-  x.forEachLink(function (link) {
-    console.log(link);
-  });
-
-})
-
-// graph.forEachLink(function(link) {
-//   console.log(link);
-
-// });
-
-
-// RouteData.GetGraph(47.660273, -122.409887, 1, 0.5, 0.6).then(result => {
-//   console.log(result);
-// })
-
-// let sunData = WeatherData.GetSunPositionToday(47.6694956, -122.31547389999999);
-// console.log(sunData);
-
-// ColorParse.GetPalette(46.414382, 10.013988, 151.78).then(colors => {
-//   console.log(colors);
-// });
-
-// ColorParse.GetPaletteNames(46.414382, 10.013988).then(result => {
-//   console.log(result);
-// })
-
-// ColorParse.GetPaletteAnalysis(47.660259, -122.408417).then(result => {
-//   console.log(result);
-// })
+module.exports = {
+  /**
+   * Field of view analysis for nature/greenery.
+   * @property {module:ColorParse}
+   */
+  ColorParse,
+  /**
+   * Street-related data for analysis.
+   * @property {module:StreetData}
+   */
+  StreetData,
+  /**
+   * Yelp-related data for analysis.
+   * @property {module:YelpData}
+   */
+  YelpData,
+   /**
+   * Weather-related data for analysis.
+   * @property {module:WeatherData}
+   */
+  WeatherData,
+   /**
+   * Route-related data for analysis.
+   * @property {module:RouteData}
+   */
+  RouteData,
+};
